20fdc1127a50293cdf02df9efbb053330f91e2ea
FrogLord-Nathaniel <145388569+FrogLord-Nathaniel@users.noreply.github.com>
2025-01-29 07:55:23 -0600

    Created starter constants

--- flatlistApp/app/index.tsx
+++ flatlistApp/app/index.tsx
@@ -9,6 +9,28 @@ import colors from "../styles/colors";
 import defaultStyles from "../styles/defaultStyles";
 
 export default function Index() {
+
+  type dataType = {
+    id: string; // Unique identifier
+    title: string; // The text we see in the flatlist
+  }
+
+  const DATA: dataType[] = [
+    {id: '1', title: 'First'},
+    {id: '2', title: 'Second'},
+    {id: '3', title: 'Third'},
+    {id: '4', title: 'Fourth'}
+  ];
+
+  /*
+  Declaring a function called selectedList that recieves
+  a param of type dataType that we will refer to as 'item'
+  I can access the values of item using dot notation 
+  */
+  const selectedList = (item: dataType) => {
+    console.log("Clicked" + item.title)
+  }
+
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
